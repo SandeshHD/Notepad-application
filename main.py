c5d15637c01fe62ffca8333712537455b645b9ab
bitt4 <64093718+bitt4@users.noreply.github.com>
2021-10-03 16:19:44 +0200

    Simplify menu creation
    This change simplifies add new labels to menu and makes adding new
    fonts easier.
    
    All you have to do is append them to the list.

--- main.py
+++ main.py
@@ -119,41 +119,46 @@ root.config(menu=mainm)
 
 file = Menu(mainm, tearoff=False)
 mainm.add_cascade(label='File', menu=file)
-file.add_command(label="New", command=new)
-file.add_command(label="Open", command=strt)
-file.add_command(label="Save", command=save)
-file.add_command(label="Save as", command=saveas)
-file.add_command(label="Exit", command=exit)
+
+file_menu_labels = ["New", "Open", "Save", "Save as", "Exit"]
+file_menu_commands = [new, strt, save, saveas, exit]
+
+for item in zip(file_menu_labels, file_menu_commands):
+    file.add_command(label=item[0], command=item[1])
+
 edit_menu = Menu(mainm, tearoff=False)
 mainm.add_cascade(label='Edit', menu=edit_menu)
-edit_menu.add_command(label="Copy", command=cpy)
-edit_menu.add_command(label="Cut", command=cut)
-edit_menu.add_command(label="Paste", command=paste)
 
 tframe = Frame(root)
 scroll = Scrollbar(tframe)
 scroll.pack(fill=Y, side=RIGHT)
 txt = Text(tframe,yscrollcommand=scroll, padx=2, pady=2, wrap=WORD, undo=True)
-edit_menu.add_command(label="Undo", command=txt.edit_undo)
-edit_menu.add_command(label="Redo", command=txt.edit_redo)
+edit_menu_labels = ["Copy", "Cut", "Paste", "Undo", "Redo"]
+edit_menu_functions = [cpy, cut, paste, txt.edit_undo, txt.edit_redo]
 
+for item in zip(edit_menu_labels, edit_menu_functions):
+    edit_menu.add_command(label=item[0], command=item[1])
 
 fontFam = Menu(mainm, tearoff=False)
 mainm.add_cascade(label='Font Family', menu=fontFam)
 
-fontFam.add_command(label="Adobe Garamond Pro", command=lambda: changeFntFam("Adobe Garamond Pro", txt))
-fontFam.add_command(label="Comic Sans MS", command=lambda: changeFntFam("Comic Sans MS", txt))
-fontFam.add_command(label="Consolas", command=lambda: changeFntFam("Consolas", txt))
-fontFam.add_command(label="Courier", command=lambda: changeFntFam("Courier", txt))
-fontFam.add_command(label="Courier New", command=lambda: changeFntFam("Courier New", txt))
-fontFam.add_command(label="Courier New Greek", command=lambda: changeFntFam("Courier New Greek", txt))
-fontFam.add_command(label="Microsoft Himalaya", command=lambda: changeFntFam("Microsoft Himalaya", txt))
-fontFam.add_command(label="Modern", command=lambda: changeFntFam("Modern", txt))
-fontFam.add_command(label="MS Sans Serif", command=lambda: changeFntFam("MS Sans Serif", txt))
-fontFam.add_command(label="Roman", command=lambda: changeFntFam("Roman", txt))
-fontFam.add_command(label="System", command=lambda: changeFntFam("System", txt))
-fontFam.add_command(label="Terminal", command=lambda: changeFntFam("Terminal", txt))
-fontFam.add_command(label="Times New Roman", command=lambda: changeFntFam("Times New Roman", txt))
+font_labels = ["Adobe Garamond Pro",
+               "Comic Sans MS",
+               "Consolas",
+               "Courier",
+               "Courier New",
+               "Courier New Greek",
+               "Microsoft Himalaya",
+               "Modern",
+               "MS Sans Serif",
+               "Roman",
+               "System",
+               "Terminal",
+               "Times New Roman"
+]
+
+for font in font_labels:
+    fontFam.add_command(label=font, command=lambda: changeFntFam(font, txt))
 
 scroll.config(command=txt.yview)
 txt.pack(fill=BOTH, expand=True)
